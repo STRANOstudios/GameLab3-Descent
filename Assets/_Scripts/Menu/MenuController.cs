73efadb2c24c651b859ebbf36422991b2a4e0fa1
STRANOstudios <friand2021@gmail.com>
2024-03-06 17:02:08 +0100

    Update

---
+++ Assets/_Scripts/Menu/MenuController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    [Header("Levels To Load")]
+    public string _newGameLevel;
+    public string levelToLoad;
+    [SerializeField] GameObject noSaveGameDialog = null;
+
+    public void NewGameDialogsYes()
+    {
+        SceneManager.LoadScene(_newGameLevel);
+    }
+
+    public void LoadGameDialogsYes()
+    {
+        if (PlayerPrefs.HasKey("SaveLevel"))
+        {
+            levelToLoad = PlayerPrefs.GetString("SavedLevel");
+            SceneManager.LoadScene(levelToLoad);
+        }
+        else
+        {
+            noSaveGameDialog.SetActive(true);
+        }
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+}
