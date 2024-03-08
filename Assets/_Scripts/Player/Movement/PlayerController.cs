9d7a480442852714015d7dfd903d2c71a664d63c
STRANOstudios <friand2021@gmail.com>
2024-03-08 22:53:04 +0100

    Update

--- Assets/_Scripts/Player/Movement/PlayerController.cs
+++ Assets/_Scripts/Player/Movement/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -5,11 +6,22 @@ public class PlayerController : MonoBehaviour
     [Header("Movement Speed")]
     [SerializeField] float Speed = 3.0f;
     [SerializeField] float multiplier = 2.0f;
+    [SerializeField] float flySpeed = 1.0f;
 
+    [SerializeField] float smoothFactor = 0.3f;
     [SerializeField] bool sprint = false;
+
+    [SerializeField] float floatingOscillationAmount = 0.5f;
+    [SerializeField] float floatingOscillationSpeed = 1f;
+    [Space]
+    [SerializeField] float bankingSpeed = 1.0f;
+    [SerializeField] float smoothness = 0.1f;
 
     [Header("Camera Settings")]
     [SerializeField] bool invertYAxis = false;
+    [SerializeField] float oscillationAmount = 1f;
+    [SerializeField] float oscillationSpeed = 1f;
+    [SerializeField] float acceleration = 1f;
 
     [Header("Look Sensitivity")]
     [SerializeField] float mouseSensitivity = 2.0f;
@@ -17,48 +29,87 @@ public class PlayerController : MonoBehaviour
 
     private CharacterController characterController;
     private Camera mainCamera;
-    private PlayerInputHadler inputHadler;
+    private PlayerInputHadler inputHandler;
     private Vector3 currentMovement;
     private float verticalRotation;
+    public bool IsBankable = false;
+
+    private float currentOscillation = 0f;
+    private float currentOscillationVelocity = 0f;
 
     private void Awake()
     {
         characterController = GetComponent<CharacterController>();
         mainCamera = Camera.main;
-        inputHadler = PlayerInputHadler.Instance;
+        inputHandler = PlayerInputHadler.Instance;
     }
 
     private void Update()
     {
-        HadlerMovement();
-        HadlerRotation();
+        HandlerMovement();
+        HandlerRotation();
+        HandlerBanking();
+        HandlerFly();
+
+        ApplyFloatingOscillation();
     }
 
-    void HadlerMovement()
+    private void HandlerFly()
     {
-        float speed = Speed * (sprint ? multiplier : 1f);
+        Vector3 flyMovement = flySpeed * inputHandler.FlyValue * transform.up;
+        characterController.Move(flyMovement * Time.deltaTime);
+    }
 
-        Vector3 inputDirection = new(inputHadler.MoveInput.x, 0f, inputHadler.MoveInput.y);
+    private void HandlerBanking()
+    {
+        if (inputHandler.bankIsActiveTrigger) IsBankable = !IsBankable;
+        if (!IsBankable) return;
+
+        float targetBankAngle = -inputHandler.bankValue * 1f * bankingSpeed;
+        Quaternion targetRotation = Quaternion.Euler(0f, 0f, targetBankAngle);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * smoothness);
+
+    }
+
+    void HandlerMovement()
+    {
+        float speedMultiplier = sprint ? multiplier : 1f;
+        float speed = Speed * speedMultiplier;
+
+        Vector2 moveInput = inputHandler.MoveInput;
+        Vector3 inputDirection = new Vector3(moveInput.x, 0f, moveInput.y).normalized;
+
         Vector3 worldDirection = transform.TransformDirection(inputDirection);
-        worldDirection.Normalize();
+        Vector3 targetMovement = worldDirection * speed;
 
-        currentMovement.x = worldDirection.x * speed;
-        currentMovement.y = worldDirection.y * speed;
-        currentMovement.z = worldDirection.z * speed;
-
+        currentMovement = Vector3.Lerp(currentMovement, targetMovement, smoothFactor * Time.deltaTime);
         characterController.Move(currentMovement * Time.deltaTime);
     }
 
-    void HadlerRotation()
+    void HandlerRotation()
     {
-        float mouseYInput = invertYAxis ? -inputHadler.LookInput.y : inputHadler.LookInput.y;
+        float mouseYInput = invertYAxis ? -inputHandler.LookInput.y : inputHandler.LookInput.y;
 
-        float mouseXRotation = inputHadler.LookInput.x * mouseSensitivity;
+        float mouseXRotation = inputHandler.LookInput.x * mouseSensitivity;
         transform.Rotate(0, mouseXRotation, 0);
 
         verticalRotation -= mouseYInput * mouseSensitivity;
-        //verticalRotation -= inputHadler.LookInput.y * mouseSensitivity;
         verticalRotation = Mathf.Clamp(verticalRotation, -upDownEange, upDownEange);
         mainCamera.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+
+        float horizontalRotation = Mathf.Abs(inputHandler.LookInput.x);
+        float targetOscillation = Mathf.Sin(Time.time * oscillationSpeed) * oscillationAmount * horizontalRotation;
+
+        currentOscillation = Mathf.SmoothDamp(currentOscillation, targetOscillation, ref currentOscillationVelocity, acceleration);
+
+        mainCamera.transform.Rotate(0, 0, currentOscillation);
+    }
+
+    void ApplyFloatingOscillation()
+    {
+        Transform mainCameraTransform = mainCamera.transform;
+        float floatingOscillation = Mathf.Sin(Time.time * floatingOscillationSpeed) * floatingOscillationAmount;
+        mainCameraTransform.localPosition = new Vector3(mainCameraTransform.localPosition.x, floatingOscillation, mainCameraTransform.localPosition.z);
     }
 }
