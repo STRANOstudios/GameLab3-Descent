8b1ba88fc2f0c93d1c78e9ef33dea5a3a943e6e2
STRANOstudios <friand2021@gmail.com>
2024-03-22 23:38:35 +0100

    Update

--- Assets/_Scripts/Manager/LevelManager.cs
+++ Assets/_Scripts/Manager/LevelManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class LevelManager : MonoBehaviour
@@ -7,13 +8,14 @@ public class LevelManager : MonoBehaviour
     [Header("Level Settings")]
     [SerializeField] GameObject reactor;
     [SerializeField] List<GameObject> Hostages;
+    [Space]
+    [SerializeField] float timeToEscape = 30f;
 
     private PlayerInputHadler inputHandler;
     private bool isGamePaused = false;
-
     private bool isReactorOpened = false;
-
     private bool pauseIsActive = true;
+    private bool successfulEscape = false;
 
     public delegate void Pause(bool value);
     public static event Pause pause = null;
@@ -31,25 +33,7 @@ public class LevelManager : MonoBehaviour
         {
             if (!reactor.activeSelf)
             {
-
-                //timer activated n.sec to escape from base
-
-                //if you can escape ...
-                if (Hostages.Count == 0)
-                {
-                    //good ending
-                    Debug.Log("good ending");
-                }
-                else
-                {
-                    //bad ending
-                    Debug.Log("bad ending");
-                }
-                //next level
-
-                //else you died
-                //return to main menu
-
+                Wait(timeToEscape);
             }
         }
     }
@@ -87,10 +71,47 @@ public class LevelManager : MonoBehaviour
         isReactorOpened = true;
     }
 
+    void Escape()
+    {
+        successfulEscape = true;
+
+        if (FreeHostage())
+        {
+            //good ending
+            Debug.Log("good ending");
+        }
+        else
+        {
+            //bad ending
+            Debug.Log("bad ending");
+        }
+
+        //return to main menu
+    }
+
+    bool FreeHostage()
+    {
+        foreach (var hostage in Hostages)
+        {
+            if (hostage.activeSelf) return false;
+        }
+        return true;
+    }
+
     private IEnumerator Delay(float value = 1f)
     {
         pauseIsActive = false;
         yield return new WaitForSecondsRealtime(value);
         pauseIsActive = true;
+    }
+
+    private IEnumerator Wait(float value = 1f)
+    {
+        yield return new WaitForSecondsRealtime(value);
+        if (!successfulEscape)
+        {
+            Debug.Log("you died");
+            //return to main menu
+        }
     }
 }
