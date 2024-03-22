7a7953976790abf9a02f98679e12faa001a64851
STRANOstudios <friand2021@gmail.com>
2024-03-22 15:14:38 +0100

    Prologue

--- Assets/_Scripts/VFX/Slides.cs
+++ Assets/_Scripts/VFX/Slides.cs
@@ -1,22 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Slides : MonoBehaviour
 {
     [Header("Slides")]
+    [SerializeField] string _newGameLevel;
+    [SerializeField] Image image;
     [SerializeField] List<Sprite> sprites = new();
     [SerializeField, Range(0f, 10f)] float delay = 1f;
 
     private void Start()
     {
-
+        if (sprites.Count <= 0) return;
+        for (int i = 0; i < sprites.Count; i++)
+        {
+            image.sprite = sprites[i];
+            Delay(delay);
+        }
+#if UNITY_EDITOR
+        SceneManager.LoadScene(_newGameLevel);
+#else
+        SceneManager.LoadScene(2); //3
+#endif
     }
 
-    private IEnumerator DelayedMiniMap(float value = 1f)
+    private IEnumerator Delay(float value = 1f)
     {
-        mapIsActive = false;
-        yield return new WaitForSeconds(value);
-        mapIsActive = true;
+        yield return new WaitForSeconds(delay);
     }
 }
